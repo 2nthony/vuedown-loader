1821a618c605be6b5d590ad59b2cecd5470978c9
evillt <ijoec123@gmail.com>
2019-07-26 00:09:00 +0800

    fix: getOptions

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const vuedown = require('vuedown')
 const { getOptions } = require('loader-utils')
 
 module.exports = function(source) {
-  const options = getOptions(this)
+  const options = getOptions(this) || {}
   return vuedown(source, options)
 }
